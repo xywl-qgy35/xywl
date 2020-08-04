2df20f6921657682fcb77b3405544d0359ee0595
xywl-qgy35 <69190205+xywl-qgy35@users.noreply.github.com>
2020-08-04 16:11:45 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,340 @@
+var local_database = [{
+    "id": 0,
+    "title": "覆盖在原生组件之上的图片视图",
+    "doctor": "张三",
+    "data": "2020-7-20 10:41",
+    "survey": "覆盖在原生组件之上的图片视图。可覆盖的原生组件同cover-view，支持嵌套在cover-view里。",
+    "body": "覆盖在原生组件之上的图片视图。可覆盖的原生组件同cover-view，支持嵌套在cover-view里。覆盖在原生组件之上的图片视图。可覆盖的原生组件同cover-view，支持嵌套在cover-view里。覆盖在原生组件之上的图片视图。可覆盖的原生组件同cover-view，支持嵌套在cover-view里。覆盖在原生组件之上的图片视图。可覆盖的原生组件同cover-view，支持嵌套在cover-view里。",
+    "shanchang": "覆盖在原生组件之上的图片视图",
+    "doctorImg": {
+      "imga": "http://127.0.0.1:84/images/new1.png",
+      "imgb": "http://127.0.0.1:84/images/bx/doctor.png"
+    },
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+  },
+  {
+    "id": 1,
+    "title": "覆盖在原生组件之上的文本视图",
+    "doctor": "李四",
+    "data": "2020-6-5 8:57",
+    "survey": "可覆盖的原生组件包括 map、video、canvas、camera、live-player、live-pusher",
+    "body": "覆盖在原生组件之上的文本视图。可覆盖的原生组件包括 map、video、canvas、camera、live-player、live-pusher只支持嵌套 cover-view、cover-image，可在 cover-view 中使用 button。组件属性的长度单位默认为px，2.4.0起支持传入单位(rpx/px)。",
+    "shanchang": "覆盖在原生组件之上的文本视图。",
+    "doctorImg": {
+      "imga": "http://127.0.0.1:84/images/new1.png",
+      "imgb": "http://127.0.0.1:84/images/bx/doctor.png"
+    },
+    
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 2,
+    "title": "小程序实现多折叠展开菜单",
+    "doctor": "王五",
+    "data": "2020-8-4 14:52",
+    "survey": "小程序实现多折叠展开菜单非常常见，想实现也非常简单，下面为大家介绍如何实现的方式",
+    "body": "微信小程序的动画只能用JS，常规手段不可用，来个头脑风暴hack起来观察每个卡片的折叠方向后，为每个卡片添加初始 rotate 使其反向折叠，我在这里定义了两个Class，另外我在 data 里设置了两个变量 isShow 和 isShowing 来判断菜单是否展开或者正在展开，来控制点击事件",
+    "shanchang": "小程序实现多折叠展开菜单效果",
+    "doctorImg": {
+      "imga": "http://127.0.0.1:84/images/new1.png",
+      "imgb": "http://127.0.0.1:84/images/bx/doctor.png"
+    },
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 3,
+    "title": "4小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框4",
+    "doctor": "赵六",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "body": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "shanchang": "用中西医两法冶疗各类不孕症如宫腔粘连、子宫发育畸形、复发流产等。",
+    "doctorImg": "http://127.0.0.1:84/images/bx/doctor.png",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 4,
+    "title": "5小程序border-radius右边失效",
+    "doctor": "胜七",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效5",
+    "body": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "shanchang": "用中西医两法冶疗各类不孕症如宫腔粘连、子宫发育畸形、复发流产等。",
+    "doctorImg": "http://127.0.0.1:84/images/bx/doctor.png",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 5,
+    "title": "6小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框6",
+    "doctor": "钱八",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+  },
+  {
+    "id": 6,
+    "title": "7小程序border-radius右边失效",
+    "doctor": "老九",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效7",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 7,
+    "title": "8小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框8",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 8,
+    "title": "9小程序border-radius右边失效",
+    "doctor": "张三",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 9,
+    "title": "10小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 10,
+    "title": "11小程序border-radius右边失效",
+    "doctor": "张三",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 11,
+    "title": "12小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 12,
+    "title": "13小程序border-radius右边失效",
+    "doctor": "张三",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 13,
+    "title": "14小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 14,
+    "title": "15小程序border-radius右边失效",
+    "doctor": "张三",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 15,
+    "title": "16小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 16,
+    "title": "17小程序border-radius右边失效",
+    "doctor": "张三",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 17,
+    "title": "18小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+  },
+  {
+    "id": 18,
+    "title": "19小程序border-radius右边失效",
+    "doctor": "张三",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 19,
+    "title": "21小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 20,
+    "title": "21小程序border-radius右边失效",
+    "doctor": "张三",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 21,
+    "title": "22小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 22,
+    "title": "23小程序border-radius右边失效",
+    "doctor": "张三",
+    "data": "2020-7-20",
+    "survey": "给input加border-radius,结果在开发者工具上是正常的,放在手机上预览右边就失效",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  },
+  {
+    "id": 23,
+    "title": "24小程序中圆角边框的设置_书写人生-CSDN博客_微信小程序圆角边框",
+    "doctor": "李四",
+    "data": "2020-7-16",
+    "survey": "与CSS一样,小程序中的圆角可以通过border-radius来设置",
+    "picture": {
+      "img1": "http://127.0.0.1:84/images/bx/wz1.png",
+      "img2": "http://127.0.0.1:84/images/bx/wz2.png",
+      "img3": "http://127.0.0.1:84/images/bx/wz3.png"
+    }
+
+  }
+]
+
+//
+module.exports = {
+  baoxi: local_database
+}
